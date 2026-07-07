287d2b8b3b6f8f542771cbb3d09461ccd8fc227e
Emem Ayara <ememline2014@gmail.com>
2026-07-07 22:32:22 +0100

    feat: initialize Python package and project configuration

---
+++ src/asset_pricing_lab/__init__.py
@@ -0,0 +1,8 @@
+"""
+Asset Pricing Research Laboratory
+
+An institutional-grade Python framework for asset pricing,
+factor modeling, and quantitative investment research.
+"""
+
+from .version import __version__
---
+++ src/asset_pricing_lab/version.py
@@ -0,0 +1,5 @@
+"""
+Version information for the Asset Pricing Research Laboratory.
+"""
+
+__version__ = "0.1.0"
